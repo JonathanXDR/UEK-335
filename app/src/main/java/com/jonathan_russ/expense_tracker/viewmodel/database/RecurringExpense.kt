fe543f7a57f97dad3b77817b36b78f0f53736aca
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2024-01-13 19:34:18 +0100

    feat(entity): add RecurringExpense entity

---
+++ app/src/main/java/com/jonathan_russ/expense_tracker/viewmodel/database/RecurringExpense.kt
@@ -0,0 +1,13 @@
+package com.jonathan_russ.expense_tracker.viewmodel.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recurring_expenses")
+data class RecurringExpense(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "price") val price: Float?,
+)
